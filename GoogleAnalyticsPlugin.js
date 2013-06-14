e642b5e8a1f6c3ecc4c04418c51477aeac7465b7
Steve Batcup <steve.batcup@gmail.com>
2013-06-14 10:32:20 +0100

    updated to use latest cordova.exec syntax

--- GoogleAnalyticsPlugin.js
+++ GoogleAnalyticsPlugin.js
@@ -1,19 +1,17 @@
 if (!window.GA) {
 	window.GA = {
-		trackerWithTrackingId: function(id) {
-			PhoneGap.exec("GoogleAnalyticsPlugin.trackerWithTrackingId",id);
-			//console.log("trackerWithTrackingId Initialized");
+		trackerWithTrackingId: function(id, successCallback, errorCallback) {
+			cordova.exec(successCallback, errorCallback, "GoogleAnalyticsPlugin", "trackerWithTrackingId", [id]);
 		},
-		trackView: function(pageUri) {
-			PhoneGap.exec("GoogleAnalyticsPlugin.trackView",pageUri);
-			//console.log("trackView Initialized");
+		trackView: function(pageUri, successCallback, errorCallback) {
+			cordova.exec(successCallback, errorCallback, "GoogleAnalyticsPlugin", "trackView", [pageUri]);
 		},
-		trackEventWithCategory: function(category,action,label,value) {
+		trackEventWithCategory: function(category,action,label,value, successCallback, errorCallback) {
 			var options = {category:category,
 				action:action,
 				label:label,
 				value:value};
-			PhoneGap.exec("GoogleAnalyticsPlugin.trackEventWithCategory",options);
+			cordova.exec(successCallback, errorCallback, "GoogleAnalyticsPlugin", "trackEventWithCategory", [options]);
 		},
 		hitDispatched: function(hitString) {
 			//console.log("hitDispatched :: " + hitString);
